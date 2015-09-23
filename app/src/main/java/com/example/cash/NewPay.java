80bdf907d2ef5883a24d01c63c5da29a9b1319f1
Akshay <akshay.g174@gmail.com>
2015-09-23 21:52:41 +0530

    Initial Commit

--- app/src/main/java/com/example/cash/NewPay.java
+++ app/src/main/java/com/example/cash/NewPay.java
@@ -1,19 +1,13 @@
 package com.example.cash;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-import boilingstocks.objects.JSONParser;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,9 +16,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-//import android.widget.TextView;
 import android.widget.Toast;
-import android.telephony.SmsManager;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import boilingstocks.objects.JSONParser;
+
+//import android.widget.TextView;
 
 public class NewPay extends Activity {
 
